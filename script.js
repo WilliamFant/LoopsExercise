f7bdcb23f0aafb80df87ab8675dee8269b1b871b
Will Fant <williamfant2023@gmail.com>
2023-04-11 09:44:41 -0500

    bonus

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ for(i=0; i<3; i++){
 let harryPotterMovies = 0;
 
 // 4b. Create a While Loop that adds 1 to the harryPotterMovies variable each time through the while loop until the harryPotterMovies variable has a value of 8.
-while(harryPotterMovies < 9){
+while(harryPotterMovies < 8){
     harryPotterMovies++
 }
 
@@ -28,8 +28,33 @@ console.log(harryPotterMovies);
 
 // BONUS
 // 5a. Create a const variable named hogwartsHouses and set the value of the variable to the following array items "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin".
+const hogwartsHouses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 // 5b. Using nested For Of Loops, have each house name displayed in the console/terminal one letter at a time (You will need a console log for this).
+for(i=0; i<3; i++){
+    for(y=0; y<hogwartsHouses[i].length; y++){
+        console.log(hogwartsHouses[i][y]);
+    }
+}
 // 6a. Create a const variable named quote and set the value of the variable to the following array items "Yer", "A", "Wizard", "Harry".
+const quote = ["Yer", "A", "Wizard", "Harry"];
 // 6b. Using a loop (You can decide which one you want to use) and the QUOTE array have the message "Yer A Wizard Harry" display on ONE line in the console/terminal (NOTE: Must have spaces between the words when it is displayed. Also, You will need a console log for this)(HINT: You will need to create another variable)
+let what = "";
+for(i=0; i<quote.length; i++){
+    what = what + quote[i] + " "
+};
+console.log(what);
+
 // 7. Create a For Loop that displays numbers 1 to 25 (AKA Counts from 1 to 25) in the terminal/console when the file is run. However, for numbers that are multiples of 3 (3, 6, 9, etc.) have the string value of "Expecto" display instead of the number. Also, for numbers that are multiples of 5 (5, 10, 15, etc.) have the string value of "Patronum" display instead of the number. Finally, for numbers that are multiples of 3 & 5 have the string value of "Expecto Patronum" display instead of the number.
+for(i=1; i<26; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("Expecto Patronum");
+    }else if(i % 3 == 0){
+        console.log("Expecto");
+    }else if(i % 5 == 0){
+        console.log("Patronum");
+    }else {
+        console.log(i);
+    };
+}
+
 // When finished, create a repository in the “LoopsExercise” Folder as well as on GitHub. Add and commit everything in the LoopsExercise folder and push it up to your GitHub repository. Then paste the link to your GitHub repository page in the form in google classroom.
